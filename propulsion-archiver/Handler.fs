3e1e7f7de65c5cb2cd039822ff29dd94a058d03f
Ruben Bartelink <ruben@bartelink.com>
2020-08-13 13:49:01 +0100

    wip

--- propulsion-archiver/Handler.fs
+++ propulsion-archiver/Handler.fs
@@ -6,11 +6,14 @@ type Stats(log, statsInterval, stateInterval) =
     override __.HandleOk(()) = ()
     override __.HandleExn exn = log.Information(exn, "Unhandled")
 
+let (|Archivable|NotArchivable|) = function
+    | "LokiPickTicketReservations" | "LokiDcBatch" | "LokiDcTransmissions" -> Archivable
+    | _ -> NotArchivable
+
 let transformOrFilter (changeFeedDocument: Microsoft.Azure.Documents.Document) : Propulsion.Streams.StreamEvent<_> seq = seq {
     for batch in Propulsion.Cosmos.EquinoxCosmosParser.enumStreamEvents changeFeedDocument do
-        match batch.stream with
-        | FsCodec.StreamName.CategoryAndId ("LokiPickTicketReservations", _) -> yield batch
-        | FsCodec.StreamName.CategoryAndId ("LokiDcBatch", _) -> yield batch
-        | FsCodec.StreamName.CategoryAndId ("LokiDcTransmissions", _) -> yield batch
-        | FsCodec.StreamName.CategoryAndId (_, _) -> ()
+        let (FsCodec.StreamName.CategoryAndId (cat,_)) = batch.stream
+        match cat with
+        | Archivable -> yield batch
+        | NotArchivable -> ()
 }
